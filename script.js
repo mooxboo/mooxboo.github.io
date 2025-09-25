a6ce1b520645786b290e57894a21b17ed3d524c8
Moo <78027558+mooxboo@users.noreply.github.com>
2025-09-25 22:56:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create the success message
         const successHTML = `
             <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTFweDFwNHJ0d2tpYnBzbzkyNGhpbXp0cjVpNGxubHJsMTRmem15ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pqxXb2to/giphy.gif" alt="Cute hug gif" class="gif">
-            <h1>YEAAYYYY ASIIKKKKK AYCE!</h1>
-            <p style="font-size: 1.2rem; color: #333;">I love you to infinity and beyond Boo Boo Sayang!</p>
+            <h1>YEAAYYYY ASIIKKKKK AYCE!!</h1>
+            <p style="font-size: 1.2rem; color: #333;">I love you to infinity and beyond Boo Boo Sayang!!</p>
         `;
 
         // Replace the container's content with the success message
